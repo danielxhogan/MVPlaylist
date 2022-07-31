384044551a011fa580d46df68ad976a0bbe50624
danielxhogan <danielxhogan@gmail.com>
2022-07-31 02:35:07 -0700

    reformat iframe

--- components/MVPlaylist/VideoWindow.jsx
+++ components/MVPlaylist/VideoWindow.jsx
@@ -125,8 +125,17 @@ export default function VideoWindow({
 
           </form>
         }
+
         {video && video.videoId &&
-          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${video.videoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
         }
 
       </div>
