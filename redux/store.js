86eb98a37ab78fb7e09e4bbcf4bd5db2b9aec865
danielxhogan <danielxhogan@gmail.com>
2022-12-21 16:21:26 -0800

    activate redux dev tools in production

--- redux/store.js
+++ redux/store.js
@@ -4,7 +4,8 @@ import ThunkMiddleware from 'redux-thunk';
 import reducers from './reducers';
 
 const bindMiddleware = (middleware) => {
-  if (process.env.NODE_ENV !== 'production') {
+  // if (process.env.NODE_ENV !== 'production') {
+  if (true) {
     const { composeWithDevTools } = require('redux-devtools-extension');
     return composeWithDevTools(applyMiddleware(...middleware));
   }
