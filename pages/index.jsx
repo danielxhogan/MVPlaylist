f8918dae79eca63ac812446ba69ef547032053f4
danielxhogan <danielxhogan@gmail.com>
2022-12-21 16:02:37 -0800

    redirect from home to playlists page if user is logged in

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,3 +9,19 @@ export default function HomePage() {
     </div>
   );
 }
+
+export const getServerSideProps =
+wrapper.getServerSideProps(store => async ({ req, res }) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/playlists',
+        permanent: false
+      }
+    };
+  }
+
+  await store.dispatch(getAllPlaylistsAction(session.accessToken));
+});
